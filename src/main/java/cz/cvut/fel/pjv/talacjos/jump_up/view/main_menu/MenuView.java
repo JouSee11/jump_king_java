914c7259b609218ed2238060953a281582215bea
Josef <pepa.talac@gmail.com>
2025-04-13 16:23:33 +0200

    adding animation on the background and adding custom button images

--- src/main/java/cz/cvut/fel/pjv/talacjos/jump_up/view/main_menu/MenuView.java
+++ src/main/java/cz/cvut/fel/pjv/talacjos/jump_up/view/main_menu/MenuView.java
@@ -11,6 +11,9 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
 
 public class MenuView {
     private final Scene scene;
@@ -36,6 +39,8 @@ public class MenuView {
     private StackPane renderMenuView() {
         StackPane root = new StackPane();
 
+        MediaView backgroundVideo = getBackgroundVideo();
+
         //background images
         Image backgroundImage = new Image(getClass().getResource("/images/main_menu/menu_background.png").toExternalForm());
         ImageView backgroundView = new ImageView(backgroundImage);
@@ -50,27 +55,35 @@ public class MenuView {
         title.getStyleClass().add("menu_title");
 
         //menu buttons
-        Button newGameBtn = new Button("New game");
-        newGameBtn.getStyleClass().add("menu_button");
+//        Button newGameBtn = new Button("New game");
+//        newGameBtn.getStyleClass().addAll("menu-button", "menu-play");
 
-        Button loadGameBtn = new Button("Load game");
-        loadGameBtn.getStyleClass().add("menu_button");
+        ImageView startGameBtn = createMenuButton("/images/main_menu/button_start.png");
+        ImageView loadGameBtn = createMenuButton("/images/main_menu/button_load.png");
+        ImageView exitBtn = createMenuButton("/images/main_menu/button_exit.png");
 
-        Button exitBtn = new Button("Exit");
-        exitBtn.getStyleClass().add("menu_button");
+        startGameBtn.getStyleClass().add("menu-button");
+        loadGameBtn.getStyleClass().add("menu-button");
+        exitBtn.getStyleClass().add("menu-button");
+//
+//        Button loadGameBtn = new Button("Load game");
+//        loadGameBtn.getStyleClass().addAll("menu-button", "menu-load");
+//
+//        Button exitBtn = new Button("Exit");
+//        exitBtn.getStyleClass().addAll("menu-button", "menu-exit");
 
         //adding items to the vbox
-        vBox.getChildren().addAll(newGameBtn, loadGameBtn, exitBtn);
+        vBox.getChildren().addAll(startGameBtn ,loadGameBtn, exitBtn);
         vBox.setAlignment(Pos.CENTER);
 
         //adding actions to the buttons
 //        newGameBtn.setOnAction(e -> sceneController.showGameScene());
-        newGameBtn.setOnAction(e -> menuController.showLevelSelection());
-        loadGameBtn.setOnAction(e -> menuController.showLoadSelection());
-        exitBtn.setOnAction(e -> sceneController.exitGame());
+        startGameBtn.setOnMouseClicked(e -> menuController.showLevelSelection());
+        loadGameBtn.setOnMouseClicked(e -> menuController.showLoadSelection());
+        exitBtn.setOnMouseClicked(e -> sceneController.exitGame());
 
         //adding items to the root
-        root.getChildren().addAll(backgroundView, vBox);
+        root.getChildren().addAll(backgroundVideo, vBox);
 
         return root;
     }
@@ -89,5 +102,45 @@ public class MenuView {
         }
     }
 
+    private MediaView getBackgroundVideo() {
+        String videoPath = getClass().getResource("/video/background_animation.mp4").toExternalForm();
+        Media media = new Media(videoPath);
+        MediaPlayer mediaPlayer = new MediaPlayer(media);
+        MediaView mediaView = new MediaView(mediaPlayer);
+
+        mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
+        mediaPlayer.setMute(true);
+
+        mediaPlayer.setOnError(() -> {
+            System.err.println("Chyba přehrávání videa: " + mediaPlayer.getError());
+        });
+
+        mediaPlayer.setOnReady(() -> {
+            System.out.println("Video připraveno k přehrávání");
+            mediaPlayer.play();
+        });
+
+        mediaView.setFitWidth(Constants.GAME_WIDTH);
+        mediaView.setFitHeight(Constants.GAME_HEIGHT);
+
+        return mediaView;
+    }
+
+    private ImageView createMenuButton(String filePath) {
+        try {
+            Image img = new Image((getClass().getResource(filePath).toExternalForm()));
+            ImageView imgView = new ImageView(img);
+
+            imgView.setFitWidth(150);
+            imgView.setPreserveRatio(true);
+
+            return imgView;
+        } catch (Exception e) {
+            System.err.println("Image doesnt exist: " + filePath);
+            e.printStackTrace();
+            return new ImageView();
+        }
+
+    }
 
 }
